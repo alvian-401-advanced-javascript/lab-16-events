f349c3f496eb61f1e2682908df3a9c26b49827aa
AJ <alvianjoseph@gmail.com>
2019-08-11 22:32:26 -0700

    lab complete

---
+++ __tests__/test.js
@@ -0,0 +1,16 @@
+// 'use strict';
+
+// const convertBuffer = require('../lib/files.js');
+// console.log(typeof convertBuffer);
+
+// const events = require('../lib/events.js');
+
+// const emit = jest.spyOn(events, 'emit');
+
+// describe('`convertBuffer` function', () => {
+//   it('should transform its argument text to uppercase', () => {
+//     const text = 'test';
+//     const result = 'TEST';
+//     expect(convertBuffer(text)).toBe(result);
+//   });
+// });
---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const files = require('./lib/files.js');
+require('./lib/logger.js');
+
+let file = process.argv.slice(2).shift();
+
+files.alterFile(file);
